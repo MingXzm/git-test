07bd8fca5cfb6e298a342657a1b31a9a8ebb02d2
RedStar <840353519@qq.com>
2022-04-07 01:02:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author redstar
+ * @create 2022-04-07 0:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
